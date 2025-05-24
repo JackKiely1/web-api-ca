2609dc1accb71d81ffeef7e6828b6f42da46d612
Jack Kiely <20068129@mail.wit.ie>
2025-05-24 15:25:32 +0100

    Added Placeholder Pages

---
+++ react-movies/src/pages/signUpPage.jsx
@@ -0,0 +1,13 @@
+const SignUpPage = () => {
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <input placeholder="Username" /><br />
+      <input type="password" placeholder="Password" /><br />
+      <input type="password" placeholder="Repeat Password" /><br />
+      <button>Register</button>
+    </>
+  );
+};
+
+export default SignUpPage;
