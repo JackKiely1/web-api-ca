8787abcfb6e4d00e7892422577fe035edf9ea462
Jack Kiely <20068129@mail.wit.ie>
2025-05-25 11:04:27 +0100

    Updated pages from react-router to react-router-dom

--- react-movies/src/main.jsx
+++ react-movies/src/main.jsx
@@ -1,50 +1,123 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter, Route, Navigate, Routes} from "react-router";
+// import HomePage from "./pages/homePage";
+// import MoviePage from "./pages/movieDetailsPage";
+// import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+// import MovieReviewPage from "./pages/movieReviewPage";
+// import SiteHeader from './components/siteHeader'
+// import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import MoviesContextProvider from "./contexts/moviesContext";
+// import AddMovieReviewPage from './pages/addMovieReviewPage';
+// import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+// import TopRatedPage from "./pages/topRatedPage";
+// import WatchPlaylistPage  from "./pages/watchPlaylistPage";
+// import PopularPage from "./pages/popularPage";
+
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 360000,
+//       refetchInterval: 360000, 
+//       refetchOnWindowFocus: false
+//     },
+//   },
+// });
+
+
+// const App = () => {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <BrowserRouter>
+//         <SiteHeader />
+//         <MoviesContextProvider>
+//           <Routes>
+//             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+//             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+//             <Route path="/movies/:id" element={<MoviePage />} />
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+//             <Route path="*" element={ <Navigate to="/" /> } />
+//             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+//             <Route path="/movies/topRated" element={<TopRatedPage />} />
+//             <Route path="/movies/watchlist" element={<WatchPlaylistPage />} />
+//             <Route path="/movies/popular" element={<PopularPage />} />
+
+//           </Routes>
+//         </MoviesContextProvider>
+//       </BrowserRouter>
+//       <ReactQueryDevtools initialIsOpen={false} />
+//     </QueryClientProvider>
+//   );
+// };
+
+
+
+// const rootElement = createRoot( document.getElementById("root") )
+// rootElement.render(<App />);
+
+
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes} from "react-router";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import TopRatedPage from "./pages/topRatedPage";
-import WatchPlaylistPage  from "./pages/watchPlaylistPage";
+import WatchPlaylistPage from "./pages/watchPlaylistPage";
 import PopularPage from "./pages/popularPage";
 
+// new  pages
+import StartPage from "./pages/startPage";
+import LoginPage from "./pages/loginPage";
+import SignUpPage from "./pages/signUpPage";
+import ProfilePage from "./pages/profilePage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
-      refetchInterval: 360000, 
+      refetchInterval: 360000,
       refetchOnWindowFocus: false
     },
   },
 });
 
-
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <SiteHeader />
         <MoviesContextProvider>
+          <SiteHeader />
           <Routes>
+            {/* New routes */}
+            <Route path="/" element={<StartPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+
+            {/* Existing routes */}
             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
-            <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
             <Route path="/movies/topRated" element={<TopRatedPage />} />
             <Route path="/movies/watchlist" element={<WatchPlaylistPage />} />
             <Route path="/movies/popular" element={<PopularPage />} />
+            <Route path="/home" element={<HomePage />} />
 
+            {/* fallback */}
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
@@ -54,7 +127,5 @@ const App = () => {
 };
 
 
-
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
-
